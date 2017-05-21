81d03927fdbd8a50cfb4154079e36b00c8c24513
小朱 (Jeff Chu) <regionbbs@ms67.url.com.tw>
2017-05-21 11:51:59 +0800

    First Release.

--- msdnmagofflinedownloader/Program.cs
+++ msdnmagofflinedownloader/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using HtmlAgilityPack;
 
 namespace msdnmagofflinedownloader
 {
     class Program
     {
+        private static readonly WebClient webclient = new WebClient();
+
         static void Main(string[] args)
         {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(webclient.DownloadString("https://msdn.microsoft.com/magazine/msdn-magazine-issues"));
+
+            // find pdfs and chms.
+            var items = doc.DocumentNode.SelectNodes("//a[@href]");
+
+            foreach (var item in items)
+            {
+                Uri url;
+
+                try
+                {
+                    url = new Uri(item.Attributes["href"].Value);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"URL format is invalid: {item.Attributes["href"].Value}");
+                    continue;
+                }
+
+                if (item.Attributes["href"].Value.EndsWith(".pdf", StringComparison.InvariantCultureIgnoreCase))
+                    Download(url);
+                if (item.Attributes["href"].Value.EndsWith(".chm", StringComparison.InvariantCultureIgnoreCase))
+                    Download(url);
+            }
+
+            Console.WriteLine("Download completed.");
+        }
+
+        private static void Download(Uri url)
+        {
+            Console.WriteLine($"Start downloading file {url}");
+
+            try
+            {
+                webclient.DownloadFile(url, Path.GetFileName(url.LocalPath));
+            }
+            catch (WebException e)
+            {
+                var response = (HttpWebResponse)e.Response;
+                Console.WriteLine($"Can't download file {url} with HTTP status code {response.StatusCode}");
+            }
         }
     }
 }
